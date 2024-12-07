cf5338823549331d15ecc93a49826013cafa401a
Natyguimaraes <santosnath433@gmail.com>
2024-12-07 00:49:55 -0300

    first

--- src/views/components/chat.jsx
+++ src/views/components/chat.jsx
@@ -14,31 +14,32 @@ function Chat() {
   };
 
   return (
-    <><div className="chat-container">
-          <div className="chat-header">
-              <h1>Chat</h1>
-          </div>
-          <div className="chat-messages">
-              {messages.map((message, index) => (
-                  <div
-                      key={index}
-                      className={`chat-message ${message.sender === 'me' ? 'sent' : 'received'}`}
-                  >
-                      {message.text}
-                  </div>
-              ))}
-          </div>
-          <div className="chat-input">
-              <input
-                  type="text"
-                  value={newMessage}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                  placeholder="Type your message..." />
-              <button onClick={handleSendMessage}>Send</button>
-          </div>
+    <div className="chat-container">
+      <div className="chat-header">
+        <h1>Chat</h1>
       </div>
-      <FooterMenu /></>
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`chat-message ${message.sender === 'me' ? 'sent' : 'received'}`}
+          >
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+      <FooterMenu />
+    </div>
   );
-};
+}
 
 export default Chat;
